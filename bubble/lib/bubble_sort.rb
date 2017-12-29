40433002c48a6701813ca2fced1c1de8e1ae30c6
Andrew Dymond <andy.dymond@gmail.com>
2017-12-29 16:03:08 -0700

    Refactor bubble sort

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -2,15 +2,19 @@ class BubbleSort
 
   def sort(unsorted_array)
     sorted_array = unsorted_array.each do |array|
-      unsorted_array.each_with_index do |element, index|
-        if unsorted_array[index + 1].nil?
-          element
-        elsif element > unsorted_array[index + 1]
-        unsorted_array[index], unsorted_array[index + 1] = unsorted_array[index + 1], unsorted_array[index]
-        end
-      end
+      sorting_criteria(unsorted_array)
     end
     sorted_array
+  end
+
+  def sorting_criteria(unsorted_array)
+    unsorted_array.each_with_index do |element, index|
+      if unsorted_array[index + 1].nil?
+        element
+      elsif element > unsorted_array[index + 1]
+      unsorted_array[index], unsorted_array[index + 1] = unsorted_array[index + 1], unsorted_array[index]
+      end
+    end
   end
 
 end
